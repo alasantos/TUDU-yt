b9274e693e14b4c9144630e9eaf349eb15b150cd
Andre Santos <aluiz.santos@gmail.com>
2022-02-01 20:43:43 -0300

    Conectando /persistindo os dados no  Atlas MongoDB

--- database/db.js
+++ database/db.js
@@ -1,14 +1,15 @@
-const mongoose = require(mongoose);
+const mongoose = require("mongoose");
 const connectToDb = () => {
-    mongoose.connect(
-        "mongodb+srv://root:admin@tudulist.n7ppu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", 
-        {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        }
+  mongoose
+    .connect(
+      "mongodb+srv://root:admin@tudulist.n7ppu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
     )
-    .then(() =>  console.log("MongoDB Atlas conectado")
+    .then(() => console.log("MongoDB Atlas conectado"))
     .catch((err) => console.log(err));
 };
 
-module.exports = connectToDb
+module.exports = connectToDb;
