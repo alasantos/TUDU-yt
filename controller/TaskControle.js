63bacc238e35630a9ef8b31bea2f657f0f665ad5
alasantos <you@example.com>
2022-02-02 18:32:26 -0300

    Implementando a edição de tarefas

--- controller/TaskControle.js
+++ controller/TaskControle.js
@@ -1,9 +1,11 @@
+const req = require("express/lib/request");
+const res = require("express/lib/response");
 const Task = require("../models/Task");
 
 const getAllTasks = async (req, res) => {
   try {
     const tasklist = await Task.find();
-    return res.render("index", {tasklist});
+    return res.render("index", {tasklist, task: null});
   } catch (err) {
     res.status(500).send({ error: err.message });
   }
@@ -24,7 +26,15 @@ const createTask = async (req, res) => {
   }
 };
 
+const getById = async (req, res ) => {
+  const task = await Task.findOne( { _id: req.param.id });
+  const tasklist = await Task.find();
+
+  res.render("index", {task, tasklist});
+}
+
 module.exports = {
   getAllTasks,
   createTask,
+  getById,
 };
--- routes/routes.js
+++ routes/routes.js
@@ -3,5 +3,6 @@ const TaskController = require("../controller/TaskControle");
 
 routes.get("/", TaskController.getAllTasks);
 routes.post("/create", TaskController.createTask);
+routes.get("/getById/:id", TaskController.getById)
 
 module.exports = routes;
