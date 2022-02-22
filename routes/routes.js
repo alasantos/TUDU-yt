ed6a008c93c564af5f91357b3b59342cf63e4796
Andre Santos <aluiz.santos@gmail.com>
2022-02-22 19:13:05 -0300

    Rota de atualização e de encerramento de tarefa

--- routes/routes.js
+++ routes/routes.js
@@ -3,9 +3,10 @@ const TaskController = require("../controller/TaskControle");
 
 routes.get("/", TaskController.getAllTasks);
 routes.post("/create", TaskController.createTask);
-routes.get("/getById/:id/:method", TaskController.getById)
+routes.get("/getById/:id/:method", TaskController.getById);
 routes.post("/updateOne/:id", TaskController.updateOneTask);
-routes.get("/deleteOne/:id", TaskController.deleteOneTask)
+routes.get("/deleteOne/:id", TaskController.deleteOneTask);
+routes.get("/check/:id", TaskController.taskCheck);
 
 
 module.exports = routes;
